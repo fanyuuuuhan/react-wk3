0e491867c5d68bc6f2e290bf61c966b33e85f310
fanyuuuuhan <s111219045@stu.ntue.edu.tw>
2025-03-10 22:07:17 +0800

    second react

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './component/Header'
-import Footer from './component/Footer'
-import ProductList from './component/ProductList'
+import Home from './pages/Home'
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
         <>
-        <Header />
-        <ProductList />
-        <Footer />
-
-
-
-
-            
-            
-            
+            <Home />
         </>
 
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import Header from "../component/Header"
+import Footer from "../component/Footer"
+import ProductList from "../component/ProductList"
+
+function Home() {
+    return (
+        <div>
+            <Header />
+           
+            <ProductList />
+            <Footer />
+        </div>
+
+
+    )
+}
+
+export default Home
